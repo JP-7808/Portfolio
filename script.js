f37a3db0db8cba09c926406a2fe1c677e690892f
Jay Prakash <jayp7808@gmail.com>
2024-10-03 18:51:22 +0530

    Pushing all file

---
+++ script.js
@@ -0,0 +1,53 @@
+function sendMail() {
+    var tempParams = {
+        from_name: document.getElementById("fromName").value,
+        email_sender: document.getElementById("emailSender").value,
+        subject_sender: document.getElementById("subjectSender").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send('service_vc4tysf', 'template_1i4hwjf', tempParams).then(function (res){
+        alert("Success!" + res.status);
+    })
+}
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Full Stack Developer","MERN Developer","Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+        fadeOut: true,
+        
+    });
+    var typed = new Typed(".typing-2",{
+        strings: ["Full Stack Developer","MERN Developer","Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+});
+
+     // Scroll to top button script
+      const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+      btnScrollToTop.addEventListener("click", function(){
+        window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    });
